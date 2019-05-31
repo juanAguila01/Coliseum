5cb4dd963d376557f52e9e496c13b99892c2a069
Juan Aguila <juan_aguilac13@hotmail.com>
2019-05-31 15:59:25 -0300

    mejora a CriaturaAbstracta
    mejoramos la implementacion de la clase abstracta reduciendo codigo redundante.

--- CriaturaAbstracta.java
+++ CriaturaAbstracta.java
@@ -1,4 +1,47 @@
 public abstract class CriaturaAbstracta {
+    protected String nombre;
+    protected int ataque,defensa,vida,idParticipante;
+    protected boolean atacando,defendiendo;
+
+    public CriaturaAbstracta(String nombre, int ataque,int defensa,int vida, int id){
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.vida = vida;
+        this.idParticipante = id;
+        //podria dejar defendiendo = true; y permitir que la defensa
+        //deje pasar 1 de daño;
+        this.atacando = false;
+        this.defendiendo = false;
+    }
+
+    //cambia de estado atacando a true y defendiendo a false.
+    //esto planeo utilizarlo con alguna clase que intermedie entre los dos participantes.
+    public boolean atacar(){
+        this.atacando = true;
+        this.defendiendo= false;
+        return this.atacando;
+    }
+    //cambia de estado atacando a false y defendiendo a true.
+    //esto planeo utilizarlo con alguna clase que intermedie entre los dos participantes.
+    public boolean defender(){
+        this.defendiendo = true;
+        this.atacando = false;
+        return this.defendiendo;
+    }
+    //metodo para imprimir criatura
+    @Override
+    public String toString(){
+        return "Nombre: " + this.nombre + "\n" +
+        "Ataque: " + this.ataque + "\n" +
+        "Defensa: " + this.defensa + "\n" + 
+        "Vida: " + this.vida + "\n" +
+        "Id Participante: " + this.idParticipante + "\n" +
+        "Esta atacando?: " + ((this.atacando)? "si":"no") + "\n" +
+        "Esta defendiendo?: " +((this.defendiendo)? "si":"no") + "\n" +
+        "===================================================";
+    }
+    
     //metodos abastractos.
     public abstract void entrenar();
     public abstract void defensaHostil();
--- Humanoide.java
+++ Humanoide.java
@@ -2,35 +2,8 @@
  * Humanoide
  */
 public class Humanoide extends CriaturaAbstracta{
-    private String nombre;
-    private int ataque,defensa,vida,idParticipante;
-    private boolean atacando,defendiendo;
-
     public Humanoide(String nombre, int ataque,int defensa,int vida, int id){
-        this.nombre = nombre;
-        this.ataque = ataque;
-        this.defensa = defensa;
-        this.vida = vida;
-        this.idParticipante = id;
-        //podria dejar defendiendo = true; y permitir que la defensa
-        //deje pasar 1 de daño;
-        this.atacando = false;
-        this.defendiendo = false;
-    }
-
-    //cambia de estado atacando a true y defendiendo a false.
-    //esto planeo utilizarlo con alguna clase que intermedie entre los dos participantes.
-    public boolean atacar(){
-        this.atacando = true;
-        this.defendiendo= false;
-        return this.atacando;
-    }
-    //cambia de estado atacando a false y defendiendo a true.
-    //esto planeo utilizarlo con alguna clase que intermedie entre los dos participantes.
-    public boolean defender(){
-        this.defendiendo = true;
-        this.atacando = false;
-        return this.defendiendo;
+        super(nombre, ataque, defensa, vida, id);
     }
 
     //metodos abstractos heredados
@@ -38,20 +11,8 @@ public class Humanoide extends CriaturaAbstracta{
         this.ataque += 3;
         this.vida += 1;
     }
+    
     //el humanoido no tiene posee este metodo por lo que
     //queda vacio.
     public void defensaHostil(){}
-    
-    //metodo para imprimir criatura
-    @Override
-    public String toString(){
-        return "Nombre: " + this.nombre + "\n" +
-        "Ataque: " + this.ataque + "\n" +
-        "Defensa: " + this.defensa + "\n" + 
-        "Vida: " + this.vida + "\n" +
-        "Id Participante: " + this.idParticipante + "\n" +
-        "Esta atacando?: " + ((this.atacando)? "si":"no") + "\n" +
-        "Esta defendiendo?: " +((this.defendiendo)? "si":"no") + "\n" +
-        "===================================================";
-    }
 }
